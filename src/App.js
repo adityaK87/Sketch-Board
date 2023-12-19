58c6abbf05e5a672609711597416b253597d3eab
Aditya Kumar <adityapal1409@gmail.com>
2023-12-19 21:42:28 +0530

    create board component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./styles/tailwind.css";
 import Menu from "./components/Menu";
 import Toolbox from "./components/Toolbox";
+import Board from "./components/Board";
 const Home = () => {
 	return (
 		<>
 			<Menu />
 			<Toolbox />
+			<Board />
 		</>
 	);
 };
