ee4c46e6993685a0525a19c93a1bea58a39b7e7d
Aditya Kumar <adityapal1409@gmail.com>
2023-12-22 22:19:40 +0530

    create store for state management

---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import MenuReducer from "./slices/menuSlice";
+
+export const store = configureStore({
+	reducer: {
+		menu: MenuReducer,
+	},
+});
